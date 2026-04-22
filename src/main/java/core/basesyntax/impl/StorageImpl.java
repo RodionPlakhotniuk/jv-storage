520ed5561577a942244ef42eb1e63561b456ce16
RodionPlakhotniuk <raidertwk@gmail.com>
2026-04-22 17:41:49 +0200

    feat: implement custom HashMap storage

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,17 +3,58 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int MAX_SIZE = 10;
+    private int size;
+
+    private K[] keys;
+    private V[] values;
+
+    @SuppressWarnings("unchecked")
+    public StorageImpl() {
+        keys = (K[]) new Object[MAX_SIZE];
+        values = (V[]) new Object[MAX_SIZE];
+    }
+
     @Override
     public void put(K key, V value) {
+        int index = findKeyIndex(key);
+
+        if (index != -1) {
+            values[index] = value;
+            return;
+        }
+        if (size == MAX_SIZE) {
+            throw new IllegalStateException("Storage is full");
+        }
+        keys[size] = key;
+        values[size] = value;
+        size++;
     }
 
     @Override
     public V get(K key) {
-        return null;
+        int index = findKeyIndex(key);
+        if (index == -1) {
+            return null;
+        }
+        return values[index];
     }
 
     @Override
     public int size() {
+        return size;
+    }
+
+    private int findKeyIndex(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == null && key == null) {
+                return i;
+            }
+
+            if (keys[i] != null && keys[i].equals(key)) {
+                return i;
+            }
+        }
         return -1;
     }
 }
